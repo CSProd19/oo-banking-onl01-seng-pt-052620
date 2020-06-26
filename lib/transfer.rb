fc2ac5b77d48de3dfedd0570e4da714d58a0934b
Maria Connie Sanchez <msnchz04@gmail.com>
2020-06-26 20:51:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,7 @@ class Transfer
     if @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-      elsif 
+    elsif @status == "complete"
+      
   end 
 end
