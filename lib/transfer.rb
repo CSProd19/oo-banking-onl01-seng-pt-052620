9f431f9f81d6601c43885b20692b1c661947114b
Maria Connie Sanchez <msnchz04@gmail.com>
2020-06-26 20:52:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,9 @@ class Transfer
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else 
-      
+       @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end 
   end 
 end
